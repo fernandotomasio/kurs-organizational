7713806f63e84487701ee29ba963247521129694
Fernando Tomasio <fernando.tomasio@sys4b.com.br>
2023-04-15 13:41:37 -0300

    first commit

---
+++ src/main/java/br/com/jeel/kurs/organizational/KursOrganizationalApplication.java
@@ -0,0 +1,13 @@
+package br.com.jeel.kurs.organizational;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursOrganizationalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursOrganizationalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jeel/kurs/organizational/KursOrganizationalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jeel.kurs.organizational;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursOrganizationalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
